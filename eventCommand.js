f4ecbb80e033afb5f7a530fe2b6212f184be04f1
Andrew White <aeewhite@gmail.com>
2015-03-29 00:25:56 -0500

    Moved event command code to inside connect listener

--- eventCommand.js
+++ eventCommand.js
@@ -2,26 +2,28 @@
 
 var net = require('net');
 
-var stream = net.connect('listener.sock');
-
 // How to Write to Stream
 //stream.write(whatever);
 
-var eventname = process.argv[2];
-var extraInfo;
-
-process.stdin.on('readable', function() {
-	var chunk = process.stdin.read();
-	if (chunk !== null) {
-		extraInfo += chunk;
+var stream = net.connect('listener.sock',function(err){
+	if(err){
+		console.log('No Socket');
 	}
-	extraInfo.replace('\\n','\n');
-	var eventObj = {
-		'event':eventname,
-		'info':extraInfo
-	};
+	var eventname = process.argv[2];
+	var extraInfo;
 
-	stream.write(JSON.stringify(eventObj));
+	process.stdin.on('readable', function() {
+		var chunk = process.stdin.read();
+		if (chunk !== null) {
+			extraInfo += chunk;
+		}
+		var eventObj = {
+			'event':eventname,
+			'info':extraInfo
+		};
 
-	stream.end();
+		stream.write(JSON.stringify(eventObj));
+
+		stream.end();
+	});
 });
